091194cf273d0aefc9411cbe79e933764ba9d0c3
Theo Lepage <theo.lepage@epita.fr>
2021-10-23 01:38:57 +0200

    feat(train): callbacks monitor test_eer

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ from sslforslr.utils.callbacks import SVMetricsCallback
 from sslforslr.models.moco import MoCoUpdateCallback
 
 def simclr_lr_scheduler(epoch, lr):
-    activate = (epoch != 0 and epoch % 5 == 0)
+    activate = (epoch != 0 and epoch % 1 == 0)
     return lr - lr * 0.05 if activate else lr
 
 def create_callbacks(config, checkpoint_dir):
@@ -27,22 +27,27 @@ def create_callbacks(config, checkpoint_dir):
     elif config['model']['type'] == 'SimCLR':
         callbacks.append(LearningRateScheduler(simclr_lr_scheduler))
 
+    callbacks.append(SVMetricsCallback(config))
+
     callbacks.append(
         ModelCheckpoint(filepath=checkpoint_dir + '/training',
-                        monitor='val_loss',
+                        monitor='test_eer',
+                        mode='min',
                         save_best_only=True,
                         save_weights_only=True,
                         verbose=1)
     )
-
-    callbacks.append(SVMetricsCallback(config))
 
     callbacks.append(
         TensorBoard(log_dir=checkpoint_dir + '/logs/',
                     histogram_freq=1)
     )
 
-    callbacks.append(EarlyStopping(monitor='val_loss', patience=5))
+    callbacks.append(
+        EarlyStopping(monitor='test_eer',
+                      mode='min',
+                      patience=5)
+    )
 
     return callbacks
 
